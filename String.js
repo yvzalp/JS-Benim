b06407eb9c8bd9a01e17dd5a42bc18859495a59f
yvzalp <canan342544@gmail.com>
2022-10-13 01:03:35 +0300

    13.10

---
+++ String.js
@@ -0,0 +1,26 @@
+const isim ="mucteba"
+const soyad="binici"
+let yas =25
+let val
+
+val=isim+" "+soyad
+
+val =isim.concat(' ',soyad)
+val =isim.length //uzunlugu verir
+val =isim[0] // 0 ci index karakteri yazdirir
+val =isim.indexOf('e') //index no döndürür
+val =isim.substring(0,3) // o dan basla 3 e kadar al 3 dahil degil
+val =isim.slice(1,4) //substring gibi
+val =isim.slice(1)//1 den sonra tümünü aldi
+
+// vb. bakabilirsin
+
+// ters tırnak pyton da f stringe benziyor
+
+
+
+
+console.log(val)
+console.log(typeof val)  // bu sekildede type bakılabilir
+
+
